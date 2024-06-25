3cf2f9b2f36d3c3da80eb40f931a8bdcea0f572c
Suthon <suthon.s@psu.ac.th>
2024-06-25 11:07:30 +0700

    PhotoPage

---
+++ lib/widgets/photo_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class PhotoPage extends StatelessWidget {
+  const PhotoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.network(
+                  'https://api.pupasoft.com/public/profile.jpg',
+                  fit: BoxFit.cover),
+            ),
+            _buildGradient(),
+            _buildTitleAndSubtitle('abc', 'hello world')
+          ],
+        )),
+      ),
+    );
+  }
+
+  Widget _buildGradient() {
+    return Positioned.fill(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.6, 0.95],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitleAndSubtitle(String title, String subtitle) {
+    return Positioned(
+      left: 20,
+      bottom: 20,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            subtitle,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
